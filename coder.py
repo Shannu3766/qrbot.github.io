fcf5beed631a7e896eaf5c118113865b514639c6
Shannu3766 <117928956+Shannu3766@users.noreply.github.com>
2023-03-04 20:15:04 +0530

    Add files via upload

---
+++ coder.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher,executor, types
+import pyqrcode
+bot = Bot(token="6055207499:AAFwOL2Zfw17DsaDEGCcuYR9fMwUmHtyCRI")
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=["start","help"])
+async def welcome(message :types.Message):
+    await message.reply("hello world this is an qr code genrator!!!")
+@dp.message_handler(commands=["logo"])
+async def logo(message :types.Message):
+    await message.answer_photo('https://th.bing.com/th/id/OIP.paYw8yErf53kXm0-upPe7AHaLF?w=186&h=278&c=7&r=0&o=5&dpr=1.3&pid=1.7')
+    
+@dp.message_handler()
+async def df(message:types.Message):
+    #await message.answer(message.text)
+    text=pyqrcode.create(message.text)
+    text.png('code.png',scale=5)
+    await bot.send_photo(chat_id=message.chat.id, photo=open('code.png','rb'))
+
+executor.start_polling(dp)
